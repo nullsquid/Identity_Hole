3b80630e5ca6a101965a0f9d0d06d95f655541fb
Azrael <j.shepard.ramsay@gmail.com>
2015-06-05 22:37:02 -0400

    first commit

---
+++ Assets/Scripts/Character/CharMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMove : MonoBehaviour {
+	bool isGrounded = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal")>0){
+			transform.Translate(Vector2.right * Time.deltaTime * 3);
+		}
+		else if (Input.GetAxis("Horizontal")<0){
+			transform.Translate(-Vector2.right * Time.deltaTime * 3);
+		}
+	}
+}
