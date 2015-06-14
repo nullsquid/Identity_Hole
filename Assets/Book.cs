dd47d780b98686fe835e8c11af400cba9359628d
Hyacinth <j.shepard.ramsay@gmail.com>
2015-06-14 15:53:23 -0400

    added timer

--- Assets/Book.cs
+++ Assets/Book.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class Book : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public int day = 1;
+	public int dayLimit = 3;
+
+	public float startTime = 0f;
+	public float timer;
+
+	public bool timerIsRunning = true;
+
+	public float timeInDay;
+
+	public World curWorld;
+
+	void Start(){
+		timer = startTime;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update(){
+		if(timerIsRunning == true){
+		timer += Time.deltaTime;
+			if(timer >= timeInDay){
+
+				Invoke("NextDay", 0);
+				timer = 0;
+			}
+		}
+		else{
+			timer = 0;
+		}
 	}
+
+	public void SwitchWorld(string worldName){
+		switch(worldName){
+
+		}
+	}
+
+	void NextDay(){
+		day += 1;
+
+	}
+
 }
--- Assets/Scripts/WorldNode.cs
+++ Assets/Scripts/WorldNode.cs
@@ -15,4 +15,6 @@ public class WorldNode : MonoBehaviour {
 		happenedSoFar.Add(null);
 	}
 
+
+
 }
